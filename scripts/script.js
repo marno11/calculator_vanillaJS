c895b22ea729fc6074aacf9c50307f726939e66c
Jordan Marano <jordan.marano11@gmail.com>
2019-01-18 13:26:45 +1000

    Differentiate Key Presses
    Key presses are now differentiated via an input method, which in-turn 
    calls the method relevant to the key pressed.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,60 @@
 var Expression = {
    exp: "",
 
+   append: function(element) {
+     this.exp = this.exp.concat(element);
+   },
+
+   removeLast: function() {
+
+   },
+
+   allClear: function() {
+
+   },
+
    evaluate: function(){
-     console.log("this is just a test");
+
+   },
+
+   input: function(key){
+     switch (key) {
+       case "1":
+       case "2":
+       case "3":
+       case "4":
+       case "5":
+       case "6":
+       case "7":
+       case "8":
+       case "9":
+       case "0":
+       case "X":
+       case "/":
+       case "+":
+       case "-":
+       case ".":
+       case "E":
+         this.append(key);
+       break;
+       case "DEL":
+         this.removeLast();
+       break;
+       case "AC":
+         this.allClear();
+       break;
+       case "ANS":
+         this.append("**previous result**");
+       break;
+       case "=":
+         this.evaluate();
+       break;
+     };
    }
 };
 
 function buttonClick(e) {
-  Expression.exp = Expression.exp.concat(e.target.innerHTML)
+  Expression.input(e.target.innerHTML)
 }
 
 const nodeListButtons = document.querySelectorAll('.row__element');
