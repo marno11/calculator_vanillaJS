057ca453627dd4af2e1365c749d1563e2ce0fa29
Jordan Marano <jordan.marano11@gmail.com>
2019-01-28 21:04:13 +1000

    Fix Callback Function from test.spec.js
    The functions calls in test.spec.js where not passing a callback 
    function where required. This commit fixes that.

--- test/test.spec.js
+++ test/test.spec.js
@@ -7,22 +7,22 @@ global.document = DOM.window.document;
 const frontEndCode = require('../scripts/script.js');
 
 test('Test append function', function(t) {
-  frontEndCode.Calculator.append("1");
+  frontEndCode.Calculator.append("1", function(){});
   let actual = frontEndCode.Calculator.exp;
   let expected = "1";
   t.equal(actual, expected, 'should append the text "1" to exp attribute of Calculator');
 
-  frontEndCode.Calculator.append("2");
+  frontEndCode.Calculator.append("2", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "12";
   t.equal(actual, expected, 'should append the text "2" to exp attribute of Calculator');
 
-  frontEndCode.Calculator.append("+");
+  frontEndCode.Calculator.append("+", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "12+";
   t.equal(actual, expected, 'should append the text "+" to exp attribute of Calculator');
 
-  frontEndCode.Calculator.append("3");
+  frontEndCode.Calculator.append("3", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "12+3";
   t.equal(actual, expected, 'should append the text "3" to exp attribute of Calculator');
@@ -32,37 +32,37 @@ test('Test append function', function(t) {
 test('Test removeLast function', function(t){
   frontEndCode.Calculator.exp = "123456";
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   let actual = frontEndCode.Calculator.exp;
   let expected = "12345";
   t.equal(actual, expected, 'should remove the "6" from "123456" and give "12345"');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "1234";
   t.equal(actual, expected, 'should remove the "5" from "12345" and give "1234"');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "123";
   t.equal(actual, expected, 'should remove the "4" from "1234" and give "123"');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "12";
   t.equal(actual, expected, 'should remove the "3" from "123" and give "12"');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "1";
   t.equal(actual, expected, 'should remove the "2" from "12" and give "1"');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'should remove the "1" from "1" and give ""');
 
-  frontEndCode.Calculator.removeLast();
+  frontEndCode.Calculator.removeLast(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'should give empty string');
@@ -72,12 +72,12 @@ test('Test removeLast function', function(t){
 test('Test allClear function', function(t){
   frontEndCode.Calculator.exp = "123456";
 
-  frontEndCode.Calculator.allClear();
+  frontEndCode.Calculator.allClear(function(){});
   let actual = frontEndCode.Calculator.exp;
   let expected = "";
   t.equal(actual, expected, 'Should clear "123456" from exp');
 
-  frontEndCode.Calculator.allClear();
+  frontEndCode.Calculator.allClear(function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'should leave an empty string empty');
@@ -94,40 +94,40 @@ test('Test input function', function(t){
 
   expressionElements.map(function(element) {
     frontEndCode.Calculator.exp = "";
-    frontEndCode.Calculator.input(element);
+    frontEndCode.Calculator.input(element, function(){});
     actual = frontEndCode.Calculator.exp;
     expected = element;
     t.equal(actual, expected, `exp attribute of Calculator should be ${element}`);
   });
 
   frontEndCode.Calculator.exp = "1234";
-  frontEndCode.Calculator.input("DEL");
+  frontEndCode.Calculator.input("DEL", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "123";
   t.equal(actual, expected, 'exp attribute of Calculator should be "123"');
 
-  frontEndCode.Calculator.input("AC");
+  frontEndCode.Calculator.input("AC", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'exp attribute of Calculator should be ""');
 
   frontEndCode.Calculator.exp = "";
-  frontEndCode.Calculator.input("a");
+  frontEndCode.Calculator.input("a", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'exp attribute of Calculator should be ""');
 
-  frontEndCode.Calculator.input("'");
+  frontEndCode.Calculator.input("'", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'exp attribute of Calculator should be ""');
 
-  frontEndCode.Calculator.input('"');
+  frontEndCode.Calculator.input('"', function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'exp attribute of Calculator should be ""');
 
-  frontEndCode.Calculator.input("");
+  frontEndCode.Calculator.input("", function(){});
   actual = frontEndCode.Calculator.exp;
   expected = "";
   t.equal(actual, expected, 'exp attribute of Calculator should be ""');
@@ -135,87 +135,87 @@ test('Test input function', function(t){
 });
 
 test("Test operate function", function(t){
-  frontEndCode.Calculator.operate("2+2");
+  frontEndCode.Calculator.operate("2+2", function(){});
   let actual = frontEndCode.Calculator.ans;
   let expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 ;
-  frontEndCode.Calculator.operate("2+2+1+4+5+7");
+  frontEndCode.Calculator.operate("2+2+1+4+5+7", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "21";
   t.equal(actual, expected, 'ans attribute of Calculator should be "21"');
 
-  frontEndCode.Calculator.operate("2+2+1+4+5+7-5-0-9-3");
+  frontEndCode.Calculator.operate("2+2+1+4+5+7-5-0-9-3", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2+4-0+5+7-5-9+2+1-3");
+  frontEndCode.Calculator.operate("2+4-0+5+7-5-9+2+1-3", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2*2");
+  frontEndCode.Calculator.operate("2*2", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2*2*1*4*5*7");
+  frontEndCode.Calculator.operate("2*2*1*4*5*7", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "560";
   t.equal(actual, expected, 'ans attribute of Calculator should be "560"');
 
-  frontEndCode.Calculator.operate("2*2*1*4*5*7/5/4");
+  frontEndCode.Calculator.operate("2*2*1*4*5*7/5/4", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "28";
   t.equal(actual, expected, 'ans attribute of Calculator should be "28"');
 
-  frontEndCode.Calculator.operate("2*4*5*7/5/4*2*1");
+  frontEndCode.Calculator.operate("2*4*5*7/5/4*2*1", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "28";
   t.equal(actual, expected, 'ans attribute of Calculator should be "28"');
 
-  frontEndCode.Calculator.operate("25+20");
+  frontEndCode.Calculator.operate("25+20", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "45";
   t.equal(actual, expected, 'ans attribute of Calculator should be "45"');
 
-  frontEndCode.Calculator.operate("25+0");
+  frontEndCode.Calculator.operate("25+0", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "25";
   t.equal(actual, expected, 'ans attribute of Calculator should be "25"');
 
-  frontEndCode.Calculator.operate("21+254+1");
+  frontEndCode.Calculator.operate("21+254+1", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "276";
   t.equal(actual, expected, 'ans attribute of Calculator should be "276"');
 
-  frontEndCode.Calculator.operate("21+254+1-5-23-111");
+  frontEndCode.Calculator.operate("21+254+1-5-23-111", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "137";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2+4-0+5+7-5-9+2+1-3");
+  frontEndCode.Calculator.operate("2+4-0+5+7-5-9+2+1-3", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2*2");
+  frontEndCode.Calculator.operate("2*2", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "4";
   t.equal(actual, expected, 'ans attribute of Calculator should be "4"');
 
-  frontEndCode.Calculator.operate("2*2*1*4*5*7");
+  frontEndCode.Calculator.operate("2*2*1*4*5*7", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "560";
   t.equal(actual, expected, 'ans attribute of Calculator should be "560"');
 
-  frontEndCode.Calculator.operate("2*2*1*4*5*7/5/4");
+  frontEndCode.Calculator.operate("2*2*1*4*5*7/5/4", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "28";
   t.equal(actual, expected, 'ans attribute of Calculator should be "28"');
 
-  frontEndCode.Calculator.operate("2*4*5*7/5/4*2*1");
+  frontEndCode.Calculator.operate("2*4*5*7/5/4*2*1", function(){});
   actual = frontEndCode.Calculator.ans;
   expected = "28";
   t.equal(actual, expected, 'ans attribute of Calculator should be "28"');
